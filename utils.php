f3ce1caa449c3040f88221681cdc4171c6697bbd
Al-Mashdali <saleh.almashdali79@gmail.com>
2022-03-10 13:40:03 +0100

    last change

--- utils.php
+++ utils.php
@@ -1,33 +1,32 @@
-<?php 
-function printFavAniImageTag() {
+<?php
+    function printFavAniImageTag(){
 
-$favoriteAnimal = $_POST["favAnimal"];
-
-
-echo "<div>";
-
-switch($favoriteAnimal) {
-    case "cat":
-        echo "<img src='/images/cat.jpg'>";
-        break;
-
-        case "Dog":
-        echo "<img src='/images/dog.jpg'>";
-        break;
-
-        case "Elephant":
-        echo "<img src='/images/elephant.jpg'>";
-        break;
+        $favoriteAnimal = $_POST["favAnimal"];
 
 
-        case "Mouse":
-        echo "<img src='/images/mouse.jpg'>";
-        break;
+        echo "<div>";
 
+        switch ($favoriteAnimal) {
+            case "cat":
+                echo "<img src='/images/cat.jpg'>";
+            break;
 
-}
+            case "Dog":
+                echo "<img src='/images/dog.jpg'>";
+            break;
 
-echo "</div>";
+            case "Elephant":
+                echo "<img src='/images/Elephant.jpeg'>";
+            break;
 
-}
+            case "Mouse":
+                echo "<img src='/images/mouse.jpg'>";
+            break;
+
+            case "Tiger":
+                echo "<img src='/images/tiger.jpg'>";
+            break;
+
+        }
+    }
 ?>
